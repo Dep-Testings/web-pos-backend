097c17176f2538a578ec9c053cb905ee2db175ef
Lucky Prabath <lucky.prabath94@gmail.com>
2020-12-21 22:01:12 +0530

    Complete doGet in itemServlet

--- src/lk/ijse/dep/web/api/ItemServlet.java
+++ src/lk/ijse/dep/web/api/ItemServlet.java
@@ -2,6 +2,7 @@ package lk.ijse.dep.web.api;
 
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
+import lk.ijse.dep.web.model.Customer;
 import lk.ijse.dep.web.model.Item;
 import org.apache.commons.dbcp2.BasicDataSource;
 
@@ -12,10 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,38 +23,37 @@ public class ItemServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        //
+        String code = req.getParameter("code");
         BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
-
-        resp.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         resp.setContentType("application/json");
-        try (PrintWriter out = resp.getWriter()) {
+        try ( Connection connection = cp.getConnection()) {
+            PrintWriter out = resp.getWriter();
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Item" + ((code != null) ? " WHERE code=?" : ""));
+            if (code != null) {
+                pstm.setObject(1, code);
+            }
+            ResultSet rst = pstm.executeQuery();
+            List<Item> itemList = new ArrayList<>();
+            while (rst.next()) {
+                code = rst.getString(1);
+                String description = rst.getString(2);
+                double unitPrice = rst.getDouble(3);
+                int qty = rst.getInt(4);
+                itemList.add(new Item(code, description, qty,unitPrice));
+            }
 
-
-            try {
-                //Class.forName("com.mysql.jdbc.Driver"); //no need this connection
-                Connection connection = cp.getConnection();
-                //Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dep6", "root", "1234");
-                Statement stm = connection.createStatement();
-                ResultSet rst = stm.executeQuery("SELECT * FROM Item");
-
-                List<Item> itemList = new ArrayList<>();
-
-                while (rst.next()) {
-                    String code = rst.getString(1);
-                    String description = rst.getString(2);
-                    double unitPrice = rst.getDouble(3);
-                    int qty = rst.getInt(4);
-                    itemList.add(new Item(code, description, qty, unitPrice));
-                }
+            if (code != null && itemList.isEmpty()) {
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            } else {
                 Jsonb jsonb = JsonbBuilder.create();
                 out.println(jsonb.toJson(itemList));
                 connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
             }
-
+        } catch (SQLException e) {
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
+
     }
 
     @Override
